fbbe688fc377c1b102412b814c73854ac046a8ce
17021084 <dotrunghg1999@gmail.com>
2020-09-09 23:50:15 +0700

    create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,28 @@ class ArticlesController < ApplicationController
   def new
   end
   def create
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
+
+    #Article refer to Article class in  /app/model
+    
+    # @article = Article.new(params[:article].permit(:title,:text))
+    
+    # recommend way ,  because it can be reuse many time
+    @article = Article.new(article_params)
+    @article.save   
+    redirect_to @article
   end
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+
+
+  end
+
 end
--- db/migrate/20200909153129_create_articles.rb
+++ db/migrate/20200909153129_create_articles.rb
@@ -1,4 +1,5 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
+  # run if we call migration
   def change
     create_table :articles do |t|
       t.string :title
