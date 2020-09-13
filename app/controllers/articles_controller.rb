db06d49f85272755b6177f417273d7b9830fb501
17021084 <dotrunghg1999@gmail.com>
2020-09-13 21:50:07 +0700

    validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,31 +2,35 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def new
+    @article = Article.new
   end
+
   def create
     # render plain: params[:article].inspect
 
-    #Article refer to Article class in  /app/model
-    
+    # Article refer to Article class in  /app/model
+
     # @article = Article.new(params[:article].permit(:title,:text))
-    
+
     # recommend way ,  because it can be reuse many time
     @article = Article.new(article_params)
-    @article.save   
-    redirect_to @article
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+
   def show
     @article = Article.find(params[:id])
   end
-  
-
 
   private
+
   def article_params
     params.require(:article).permit(:title, :text)
-
-
   end
-
 end
