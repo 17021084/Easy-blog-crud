5b18df0deeea7e980181f1be6954660080060a83
17021084 <dotrunghg1999@gmail.com>
2020-09-13 22:00:38 +0700

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,22 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     # render plain: params[:article].inspect
 
