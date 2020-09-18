98ae015df36b6bad83d8da9b3ada08a6126b4d77
17021084 <dotrunghg1999@gmail.com>
2020-09-18 09:23:04 +0700

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  
   root 'application#hello'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
