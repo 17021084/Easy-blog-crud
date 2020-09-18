8db980c1cf8dadc770df84a4c811f59124db4e48
17021084 <dotrunghg1999@gmail.com>
2020-09-18 09:42:55 +0700

    setting root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
+
+  root 'static_pages#home'    # routes localhost:3000/ <=> localhost:3000/static_route/home
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
   
-  root 'application#hello'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
