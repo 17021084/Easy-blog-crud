d0d33c879238ca5231ddaed534b762829551319d
17021084 <dotrunghg1999@gmail.com>
2020-09-26 11:01:58 +0700

    exercise 1,2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
 
-  has_secure_password
+  # has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-  root 'static_pages#home'    # routes localhost:3000/ <=> localhost:3000/static_route/home
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
   
+  root 'static_pages#home'    # routes localhost:3000/ <=> localhost:3000/static_route/home
+  # refer from short url to native long url
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  
+  # atomaticlly generate REST url
+  resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
