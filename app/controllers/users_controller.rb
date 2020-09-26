9877f6e7e1cfc194720b2f5ae6d74f13b2f0f709
17021084 <dotrunghg1999@gmail.com>
2020-09-26 12:02:58 +0700

    showing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user =User.find(params[:id])
+   
+    # debugger #return nill va co the debug luon o day , debug truc tieo
   end
   
   def new
+    # debugger
   end
 
 end
